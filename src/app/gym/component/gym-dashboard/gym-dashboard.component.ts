7d20997f01efe4675641abf126ed6ac1e15f70e9
Ravi Rajaram Sathe <ravisathe@AD.NSEIT.COM>
2024-08-04 17:30:53 +0530

    gym dashboard

--- src/app/gym/component/gym-dashboard/gym-dashboard.component.ts
+++ src/app/gym/component/gym-dashboard/gym-dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import LocomotiveScroll from 'locomotive-scroll';
+
 
 @Component({
   selector: 'app-gym-dashboard',
@@ -6,5 +8,17 @@ import { Component } from '@angular/core';
   styleUrl: './gym-dashboard.component.scss'
 })
 export class GymDashboardComponent {
+
+  locomotiveScroll: any;
+
+  ngAfterViewInit(){
+    // const scrollContainer = document.querySelector('#scroll');
+    // if (scrollContainer) {
+    //   this.locomotiveScroll = new LocomotiveScroll({
+    //     el: scrollContainer as HTMLElement, // or use scrollContainer directly
+    //     smooth: true,
+    //   });
+    // }
+  }
 
 }
